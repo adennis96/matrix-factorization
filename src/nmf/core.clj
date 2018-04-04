6b53fd6677dacb320ad211ae52e7a6bd4485a49c
adennis96 <adennis96@users.noreply.github.com>
2018-04-03 23:43:55 -0400

    adding timing and test data

--- src/nmf/core.clj
+++ src/nmf/core.clj
@@ -1,16 +1,19 @@
 (ns nmf.core
-  (:use [nmf nmf matrix]
+  (:use [nmf nmf matrix data]
         [clojure.math numeric-tower])
   (:gen-class))
 
-(def X
-  [[2 0 1 2 0] [0 2 1 0 1] [1 0 0 2 2] [2 1 0 0 1]])
-
 (defn -main
-  []
-  (let [[W H] (nmf X 4 5000)]
-    (println "X: " X)
-    (println "W: " W)
-    (println "H: " H)
-    (println "WH: " (matr-mult W H))
+  [& args]
+  (when (< (count args) 3)
+        (println "usage: lein run <matrix> <num-features> <max-iterations>")
+        (System/exit 0))
+  (let [V (eval (symbol "nmf.data" (nth args 0)))
+        k (Integer/parseInt (nth args 1))
+        max-iter (Integer/parseInt (nth args 2))
+        [W H] (nmf V k max-iter)]
+    (println "V: " V)
+    ; (println "W: " W)
+    ; (println "H: " H)
+    ; (println "WH: " (matr-mult W H))
     (println "WH(rounded): " (matr-map round (matr-mult W H)))))
--- src/nmf/nmf.clj
+++ src/nmf/nmf.clj
@@ -12,16 +12,20 @@
   [V k max-iter]
   (loop [W (rand-matr (height V) k 1)
          H (rand-matr k (width V) 1)
-         iter max-iter]
-    (println iter " " (diff-cost V (matr-mult W H))) ; DEBUG
+         iter max-iter
+         start-time (System/currentTimeMillis)]
+    (printf "step %2d; cost: %.4f\n" iter (diff-cost V (matr-mult W H))) ; DEBUG
     (if (or (<= iter 0) (= (diff-cost V (matr-mult W H)) 0))
       [W H]
       (let [new-H (matr-doall (matr-map * H
                     (matr-map / (matr-mult (transpose W) V)
-                                (matr-mult (transpose W) W H))))]
+                                (matr-mult (transpose W) W H))))
+            new-W (matr-doall (matr-map * W
+                    (matr-map / (matr-mult V (transpose new-H))
+                                (matr-mult W new-H (transpose new-H)))))]
+        (printf "time: %.4f sec; " (float (/ (- (System/currentTimeMillis) start-time) 1000))) ; DEBUG
         (recur
-          (matr-doall (matr-map * W
-            (matr-map / (matr-mult V (transpose new-H))
-                        (matr-mult W new-H (transpose new-H)))))
+          new-W
           new-H
-          (- iter 1))))))
+          (- iter 1)
+          (System/currentTimeMillis))))))
